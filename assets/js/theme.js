2614ebed51e17f4f2bda7d92bef24416c9c79ca3
David Sanchez <david38sanchez@gmail.com>
2017-04-12 11:36:02 -0400

    Add affix for `.masthead-nav`

--- assets/js/theme.js
+++ assets/js/theme.js
@@ -12,7 +12,6 @@
             return false;
         }
     }).ready(function() {
-        console.log('here');
         $(window).bind('hashchange', function(event) {
             var tgt = location.hash.replace(/^#\/?/,'');
             if ( document.getElementById(tgt) ) {
@@ -31,4 +30,60 @@
         });
         $(window).trigger('hashchange');
     });
+
+    /* ********* heronav section ********* */
+    var $mastheadNav = $('.masthead-nav');
+    if ($(window).width() > 991 ) {
+        if ($mastheadNav.length > 0) {
+            $mastheadNav.affix({
+                offset: {
+                    top:$mastheadNav.offset().top
+                }
+            });
+        }
+    } else {
+        if ($mastheadNav.length > 0) {
+            $mastheadNav.affix({
+                offset: {
+                    top:$mastheadNav.offset().top - 50
+                }
+            });
+        }
+    }
+    if ($(window).width() < 767 ) {
+        $mastheadNav.append('<a class="arrow-left arrow">L</a><a class="arrow-right arrow">R</a>');
+        var box = $(".masthead-nav ul"), x;
+        $(".arrow").click(function() {
+            if ($(this).hasClass("arrow-right")) {
+                x = ((box.width() / 2)) + box.scrollLeft();
+                box.animate({
+                    scrollLeft: x,
+                })
+            } else {
+                x = ((box.width() / 2)) - box.scrollLeft();
+                box.animate({
+                    scrollLeft: -x,
+                })
+            }
+        })
+    }
+    $('.masthead-nav ul').scroll(function() {
+        var widthtotal = 0;
+        $('.masthead-nav ul li').each(function() {
+            var $this = $(this);
+            widthtotal += $this.outerWidth(true);
+        });
+        if($(this).scrollLeft() == 0) {
+            $('.masthead-nav .arrow-left').hide();
+        }
+        else if($('.masthead-nav .arrow-left').css('display') != 'block') {
+            $('.masthead-nav .arrow-left').show();
+        }
+        if(widthtotal >= $(this).scrollLeft() + $(this).outerWidth(true)) {
+            $('.masthead-nav .arrow-right').show();
+        }
+        else if($('.masthead-nav .arrow-right').css('display') != 'none') {
+            $('.masthead-nav .arrow-right').hide();
+        }
+    }).scroll();
 })(jQuery);
